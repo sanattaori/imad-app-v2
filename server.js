9e01915c8466dd7b7d18d5bc5d734cc61666bcc9
sanattaori <sanattaori3@gmail.com>
2017-04-02 17:57:07 +0530

    initialized

--- server.js
+++ server.js
@@ -5,17 +5,87 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/tes-dbt',function(req, res){
+    
+}); 
+
+app.get('/maps', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'maps.html'));
+});
+
+app.get('/voice', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'voice.html'));
+});
+
+
+
+var counter = 0;
+app.get('/counter', function (req, res){
+    counter = counter +1;
+    res.send (counter.toString());
+    
+});
+
+
+app.get('/ui/particle.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'particle.js'));
+});
+
+app.get('/ui/app.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'app.js'));
+});
+
+var names=[];
+app.get('/submit-name', function (req, res) {
+  var name= req.query.name;
+  names.push(name);
+  res.send(JSON.stringify(names));
+});
+
+
+app.get('/article-one', function (req, res){
+    res.send('Artical one requested');
+    
+});
+
+app.get('/article-two', function (req, res){
+  res.send(createTemplate(articleOne));
+    
+});
+
+app.get('/article-three', function (req, res){
+    res.send('Artical three requested');
+    
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/typed.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'typed.js'));
+});
+
+app.get('/ui/magic.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'magic.css'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,104 @@
 console.log('Loaded!');
+
+
+
+/*-------------------------------google sign in-------------------
+*/
+
+
+/*-------------------------------*/
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+
+
+$('.madi2').hover(function () {
+  $(this).addClass('magictime vanishIn');
+});
+setTimeout(function(){
+     $('.madi2').addClass('magictime puffIn');
+ }, 2000);
+
+
+
+
+var button = document.getElementById('counter');
+
+button.onclick = function() {
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if (request.readyState ===XMLHttpRequest.DONE){
+            if(request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://sanattaori.imad.hasura-app.io/counter',true);
+    request.send(null);
+
+};
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+alert('click on sign in button to post your comment');
+}
+
+//submit
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+    var gname = profile.getName();
+    
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    var request = new XMLHttpRequest();
+     
+    request.onreadystatechange = function() {
+        if (request.readyState ===XMLHttpRequest.DONE){
+            if(request.status === 200) {
+               var names = request.responseText;
+               names = JSON.parse(names);
+    var list ='';
+    for (var i=0; i<names.length; i++) {
+        list+='<li>' + names[i] + '</li>' +'</br>'
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+            }
+        }
+    };
+    var space =' :-'+'</br>';
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET','http://sanattaori.imad.hasura-app.io/submit-name?name=' + gname + space + name ,true);
+    request.send(null);
+    
+    
+};
+
+};
+//loder
+
+$(document).ready(function() {
+ 
+  // Fakes the loading setting a timeout
+    setTimeout(function() {
+        $('body').addClass('loaded');
+    }, 3500);
+ 
+});
+  
+
+
+
+setTimeout(function(){
+     $('.madi2').addClass('magictime puffIn');
+ }, 5000);
