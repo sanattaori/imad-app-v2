9d07ee9d26bd88ac1cb8b4a90f91f46c8cd02290
Sanat Taori <sanattaori@users.noreply.github.com>
2017-04-04 12:19:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var crypto = require('crypto');
 var Pool = require('pg').Pool;
 
 var config = {
@@ -21,6 +21,18 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input,salt) {
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+
+app.get('/hash/:input',function(req,res){
+    var hashedString = hash(req.params.input,'this-is-random-string');
+    res.send(hashedString);
+    
 });
 
 var pool = new Pool(config);
