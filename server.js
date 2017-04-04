3490934503db53326ca919ce42301936d7e9488b
Sanat Taori <sanattaori@users.noreply.github.com>
2017-04-04 12:43:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var crypto = require('crypto');
 var Pool = require('pg').Pool;
-
+var bodyParser = require('body-parser');
 var config = {
     
     user: 'sanattaori',
@@ -16,7 +16,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
-
+app.use(bodyParser.json());
  
 
 app.get('/', function (req, res) {
@@ -34,6 +34,27 @@ app.get('/hash/:input',function(req,res){
     res.send(hashedString);
     
 });
+
+app.post('/create-user-n',function(req,res){
+//username,password 
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    var salt = crypto.getRandomBytes(128).toString('hex');
+    var dbstring = hash(password,salt);
+    pool.query('INSERT INTO "user"(username,password) VALUES($1,$2)',[username,dbString,function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send('User successfully created: '+ username);
+        }
+        }
+    }]) 
+
+    
+});
+
 
 var pool = new Pool(config);
 
