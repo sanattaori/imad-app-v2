7f1f81276bde5d1c79d2a759d6354b5fc824561d
Sanat Taori <sanattaori@users.noreply.github.com>
2017-04-05 12:55:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,26 +24,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-//////////////////////////////////////////////////////
 
-const decipher = crypto.createDecipher('aes192', 'a password');
-
-let decrypted = '';
-decipher.on('readable', () => {
-  const data = decipher.read();
-  if (data)
-    decrypted += data.toString('utf8');
-});
-decipher.on('end', () => {
-  console.log(decrypted);
-  // Prints: some clear text data
-});
-
-const encrypted = 'ca981be48e90867604588e75d04feabb63cc007a8f8ad89b10616ed84d815504';
-decipher.write(encrypted, 'hex');
-decipher.end();
-
-//////////////////////////////////////////////////////
 
 
 function hash(input,salt) {
