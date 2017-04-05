5ae1ef6c1bcce09b2d3e9006b3042793231964f7
Sanat Taori <sanattaori@users.noreply.github.com>
2017-04-05 17:42:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,13 @@
 console.log('Loaded!');
 
-
+var x = document.getElementById('hideit');
 
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
+      x.style.display = 'none';
     });
   }
 
@@ -46,7 +47,7 @@ submit.onclick = function(){
 alert('click on sign in button to post your comment');
 }
 
-  var x = document.getElementById('hideit');
+  
  x.style.display = 'none';
 //submit
 function onSignIn(googleUser) {
